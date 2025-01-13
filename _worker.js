f587cb5a9c67117d4c01a2133d3f8877fe87fcda
mosky759 <nocolazef08@gmail.com>
2025-01-13 18:24:19 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'd8d6cda9-7268-48aa-8c10-03f1ef76c353,79a33e55-38eb-46e7-9b0e-3fcc5cfd5138,adc010e4-37ba-4650-9df1-8c2144a936d2';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
